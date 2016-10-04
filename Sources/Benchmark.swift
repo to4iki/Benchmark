d7e135aa88c96c3d0f9e01c8ada4806b719751e1
tsk takezawa <tsk.take815@gmail.com>
2016-10-05 01:04:53 +0900

    Add Benchmark.bm, bm

--- Sources/Benchmark.swift
+++ Sources/Benchmark.swift
@@ -1,4 +1,24 @@
-struct Benchmark {
+import Foundation
 
-    var text = "Hello, World!"
+public struct Benchmark {
+
+    private init() {}
+    
+    public static func bm(f: (Report) -> Void) {
+        let report = Report()
+        f(report)
+    }
+    
+    public static func bm(labels: [String], f: (Report) -> [TimeInterval]) {
+        let report = Report()
+        let timeIntervals = f(report)
+        zip(labels, timeIntervals).map(Times.init).forEach { print($0) }
+    }
+    
+    public static func measure(label: String = "", f: () -> Void) -> Times {
+        let startTime = Date()
+        f()
+        let elapsed = Date().timeIntervalSince(startTime)
+        return Times(label: label, realTime: elapsed)
+    }
 }
