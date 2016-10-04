9c5857b069d9dfd5400897ad95c515b6e26d3f29
tsk takezawa <tsk.take815@gmail.com>
2016-10-05 01:08:19 +0900

    Add mesure tests

--- Tests/BenchmarkTests/BenchmarkTests.swift
+++ Tests/BenchmarkTests/BenchmarkTests.swift
@@ -1,17 +1,42 @@
 import XCTest
 @testable import Benchmark
 
-class BenchmarkTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(Benchmark().text, "Hello, World!")
+final class BenchmarkTests: XCTestCase {
+    
+    func testBm() {
+        measure {
+            Benchmark.bm(labels: ["total: ", "ave: "]) { (r: Benchmark.Report) -> [TimeInterval] in
+                let one = r.output(label: "one: ") { self.fibonacci(30) }
+                let two = r.output(label: "two: ") { self.fibonacci(30) }
+                let three = r.output(label: "three: ") { self.fibonacci(30) }
+                let total = one.elapsed + two.elapsed + three.elapsed
+                let ave = total / 3
+                return [total, ave]
+            }
+        }
     }
-
-
+    
+    func testMeasure() {
+        measure {
+            Benchmark.measure { self.fibonacci(30) }
+        }
+    }
+    
     static var allTests : [(String, (BenchmarkTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testBm", testBm),
+            ("testMeasure", testMeasure)
         ]
+    }
+}
+
+extension BenchmarkTests {
+    
+    fileprivate func fibonacci(_ n: UInt) -> UInt {
+        if n <= 1 {
+            return n
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2)
+        }
     }
 }
